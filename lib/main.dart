50a76a7b8a1d0624dcc0339b6443fc0ebe883091
Matheus Rato Matias <matias135235@gmail.com>
2023-11-20 00:37:25 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,22 +22,23 @@ import 'package:sigalogin/src/repositories/update_repository.dart';
 import 'package:sigalogin/src/services/notification_service.dart';
 import 'package:sigalogin/src/services/update_service.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitDown,
-    DeviceOrientation.portraitUp
-  ]);
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
 
   SharedPreferences prefs = await SharedPreferences.getInstance();
   UpdateService updateService = UpdateService();
-  runApp(
-      ChangeNotifierProvider(create:  (context)=>SettingRepository(prefs: prefs),child: MyApp(page: const SplashPage()),)
-  );
+  runApp(ChangeNotifierProvider(
+    create: (context) => SettingRepository(prefs: prefs),
+    child: MyApp(page: const SplashPage()),
+  ));
 
   Update? update;
 
-  try{update = await updateService.verifyAvailableUpdate();}finally{}
+  try {
+    update = await updateService.verifyAvailableUpdate();
+  } finally {}
 
   StudentController control = StudentController();
   StudentCardController cardControl = StudentCardController();
@@ -48,46 +49,27 @@ void main() async{
   List<Historic> historic = await control.queryHistoric();
   StudentCard card = await cardControl.queryDatabase();
 
-  if(student.cpf == ''){
-    runApp(
-        MultiProvider(
-          providers: [
-            ChangeNotifierProvider<SettingRepository>(create: (context)=>SettingRepository(prefs: prefs)),
-            ChangeNotifierProvider<StudentRepository>(create: (context)=>StudentRepository(student, [], [], [])),
-            ChangeNotifierProvider<StudentCardRepository>(create: (context)=>StudentCardRepository(card)),
-            ChangeNotifierProvider<UpdateRepository>(create: (context)=>UpdateRepository(update: update)),
-            Provider<NotificationService>(create: (context)=>NotificationService())
-          ],
-          child: MyApp(page: const LoginPage()),
-        )
-    );
-  }else{
-    if(prefs.getBool('appLock')??false){
-      runApp(
-          MultiProvider(
-            providers: [
-              ChangeNotifierProvider<SettingRepository>(create: (context)=>SettingRepository(prefs: prefs)),
-              ChangeNotifierProvider<StudentRepository>(create: (context)=>StudentRepository(student,historic,assessment,schedule)),
-              ChangeNotifierProvider<StudentCardRepository>(create: (context)=>StudentCardRepository(card)),
-              ChangeNotifierProvider<UpdateRepository>(create: (context)=>UpdateRepository(update: update)),
-              Provider<NotificationService>(create: (context)=>NotificationService())
-            ],
-            child: MyApp(page: const AuthPage()),
-          )
-      );
-    }else{
-      runApp(
-          MultiProvider(
-            providers: [
-              ChangeNotifierProvider<SettingRepository>(create: (context)=>SettingRepository(prefs: prefs)),
-              ChangeNotifierProvider<StudentRepository>(create: (context)=>StudentRepository(student,historic,assessment,schedule)),
-              ChangeNotifierProvider<StudentCardRepository>(create: (context)=>StudentCardRepository(card)),
-              ChangeNotifierProvider<UpdateRepository>(create: (context)=>UpdateRepository(update: update)),
-              Provider<NotificationService>(create: (context)=>NotificationService())
-            ],
-            child: MyApp(page: HomePage(afterLogin: !(prefs.getBool('updateOnOpen')??true), update: update)),
-          )
-      );
-    }
-  }
+  Widget page = const LoginPage();
+
+  if (student.cpf != '')
+    page = HomePage(
+        afterLogin: !(prefs.getBool('updateOnOpen') ?? true), update: update);
+  if (prefs.getBool('appLock') ?? false) page = const AuthPage();
+  
+  runApp(MultiProvider(
+        providers: [
+          ChangeNotifierProvider<SettingRepository>(
+              create: (context) => SettingRepository(prefs: prefs)),
+          ChangeNotifierProvider<StudentRepository>(
+              create: (context) =>
+                  StudentRepository(student, historic, assessment, schedule)),
+          ChangeNotifierProvider<StudentCardRepository>(
+              create: (context) => StudentCardRepository(card)),
+          ChangeNotifierProvider<UpdateRepository>(
+              create: (context) => UpdateRepository(update: update)),
+          Provider<NotificationService>(
+              create: (context) => NotificationService())
+        ],
+        child: MyApp(page: page),
+      ));
 }
